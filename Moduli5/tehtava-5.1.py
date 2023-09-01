d2bfccd95044118852e2897140c4d919bae58a7d
alextoro306 <alextoro306@gmail.com>
2023-09-01 13:22:54 +0300

    moduli5 tekoa

--- Moduli5/tehtava-5.1.py
+++ Moduli5/tehtava-5.1.py
@@ -1,4 +1,8 @@
+import random
+
 arpakuutio = int(input("Anna arpakuutioiden lukumäärä: "))
 
-for arpakuutio in range(1, 6):
-    print(arpakuutio)
+for arpakuutio in range(arpakuutio):
+    arpakuutioarvo = random.randint(1, 6)
+    vastaus = arpakuutioarvo + arpakuutioarvo
+    print(f"arpakuutioiden arvo on: {vastaus}")
