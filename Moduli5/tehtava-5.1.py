b6d6feb3b62ecd8c25f9919d382d0ab507ce9dba
alextoro306 <alextoro306@gmail.com>
2023-09-01 13:38:00 +0300

    5.1 done

--- Moduli5/tehtava-5.1.py
+++ Moduli5/tehtava-5.1.py
@@ -2,7 +2,11 @@ import random
 
 arpakuutio = int(input("Anna arpakuutioiden lukumäärä: "))
 
+summa = 0
+
 for arpakuutio in range(arpakuutio):
     arpakuutioarvo = random.randint(1, 6)
-    vastaus = arpakuutioarvo + arpakuutioarvo
-    print(f"arpakuutioiden arvo on: {vastaus}")
+    summa += arpakuutioarvo
+    print(f"arpakuutioiden arvo on: {arpakuutioarvo}")
+
+print(f"arpakuutioiden summa on: {summa}")
