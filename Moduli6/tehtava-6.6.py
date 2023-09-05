21992dc95f0273b09ea8c6a857489b2b3569fbfc
alex <alextoro306@gmail.com>
2023-09-05 19:32:51 +0300

    moduli6 done

--- Moduli6/tehtava-6.6.py
+++ Moduli6/tehtava-6.6.py
@@ -1,18 +1,17 @@
 import math
 
 def main():
-        kutsuttu = 1
-        pizzaHalkaisija1 = input("Syötä ensimmäisen pizzan halkaisija: ")
-        pizzaHalkaisija2 = input("Syötä toisen pizzan halkaisija: ")
-        pizzaHinta1 = input("Syötä ensimmäisen pizzan hinta: ")
-        pizzaHinta2 = input("Syötä toisen pizzan hinta: ")
-        if pizzaHalkaisija1 !="" and pizzaHalkaisija2 !="" and pizzaHinta1 !="" and pizzaHinta2 !="":
-            pizza1 = yksikkoHinta(float(pizzaHalkaisija1), float(pizzaHinta1), kutsuttu)
-            kutsuttu +=1
-            pizza2 = yksikkoHinta(float(pizzaHalkaisija2), float(pizzaHinta2), kutsuttu)
+    kutsu = 1
+    pizzaHalkaisija1 = input("Syötä ensimmäisen pyöreän pizzan halkaisija: ")
+    pizzaHinta1 = input("Syötä ensimmäisen pyöreän pizzan hinta: ")
+    pizzaHalkaisija2 = input("Syötä toisen pyöreän pizzan halkaisija: ")
+    pizzaHinta2 = input("Syötä toisen pyöreän pizzan hinta: ")
+    if pizzaHalkaisija1 != "" and pizzaHalkaisija2 != "" and pizzaHinta1 != "" and pizzaHinta2 != "":
+        pizza1 = yksikkoHinta(float(pizzaHalkaisija1), float(pizzaHinta1), kutsu)
+        kutsu +=1
+        pizza2 = yksikkoHinta(float(pizzaHalkaisija2), float(pizzaHinta2), kutsu)
 
 def yksikkoHinta(halkaisija, hinta, kutsuttu):
-
     area = (math.pi * (((halkaisija/100) / 2) ** 2))
     yksikkohinta = hinta/area
     hinta = float(round(yksikkohinta, 2))
