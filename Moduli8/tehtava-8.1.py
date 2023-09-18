40740e653aef329ed90c12b40cf04cba376d0197
alextoro306 <alextoro306@gmail.com>
2023-09-18 10:46:12 +0300

    moduli8 eka teht valmis

---
+++ Moduli8/tehtava-8.1.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='halorvalor',
+         autocommit=True
+         )
+
+def haeMaakoodi(icaokoodi):
+    sql = "SELECT name, iso_region FROM airport "
+    sql += " WHERE ident = '" + icaokoodi + "' "
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0:
+        for i in tulos:
+            print(i)
+    return
+
+icaokoodi = input("Anna ICAO-koodi: ")
+haeMaakoodi(icaokoodi)
