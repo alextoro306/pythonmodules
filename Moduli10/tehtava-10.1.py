51502c5df067d4693f0be3a278b28a079fa285ce
alex <alextoro306@gmail.com>
2023-10-25 09:44:11 +0300

    moduli10 done

--- Moduli10/tehtava-10.1.py
+++ Moduli10/tehtava-10.1.py
@@ -1,21 +1,26 @@
-"""
-Kirjoita Hissi-luokka, joka saa alustajaparametreinaan alimman ja ylimmän kerroksen numeron.
-Hissillä on metodit siirry_kerrokseen, kerros_ylös ja kerros_alas.
-Uusi hissi on aina alimmassa kerroksessa.
-Jos tee luodulle hissille h esimerkiksi metodikutsun h.siirry_kerrokseen(5),
-metodi kutsuu joko kerros_ylös- tai kerros_alas-metodia niin monta kertaa, että hissi päätyy viidenteen kerrokseen.
-Viimeksi mainitut metodit ajavat hissiä yhden kerroksen ylös- tai alaspäin ja ilmoittavat, missä kerroksessa hissi sen jälkeen on.
-Testaa luokkaa siten, että teet pääohjelmassa hissin ja käsket sen siirtymään haluamaasi kerrokseen ja sen jälkeen takaisin alimpaan kerrokseen.
-"""
-
 class Hissi:
-    def __init__(self, alinkerros, ylinkerros):
-        self.alinkerros = alinkerros
-        self.ylinkerros = ylinkerros
-        self.kerros = 1
-
-    def siirry_kerrokseen(self):
+    def __init__(self, alin_kerros, ylin_kerros):
+        self.kerros = alin_kerros
+        self.alin_kerros = alin_kerros
+        self.ylin_kerros = ylin_kerros
 
     def kerros_ylös(self):
+        if self.kerros < self.ylin_kerros:
+            self.kerros += 1
+        print(f"Hissi on kerroksessa {self.kerros}")
 
     def kerros_alas(self):
+        if self.kerros > self.alin_kerros:
+            self.kerros -= 1
+        print(f"Hissi on kerroksessa {self.kerros}")
+
+    def siirry_kerrokseen(self, kohde_kerros):
+        while self.kerros < kohde_kerros:
+            self.kerros_ylös()
+        while self.kerros > kohde_kerros:
+            self.kerros_alas()
+        print(f"Hissi on nyt kerroksessa {self.kerros}")
+
+hissi = Hissi(1, 10)
+hissi.siirry_kerrokseen(1)
+hissi.siirry_kerrokseen(5)
